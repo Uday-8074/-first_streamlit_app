7ea368a956353ee7ee44144940e88b88057217d2
Uday-8074 <148123198+Uday-8074@users.noreply.github.com>
2023-10-16 23:15:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Diner') 
 streamlit.header('Breakfast Menu')
-streamlit.text('Grape Juice')
-streamlit.text('Banana-Juice')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
